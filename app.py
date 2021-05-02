f6b6e54084b27710cbf07ce486d69764f52d8910
Nitish Prabhu Kota <nitish.prabhu26@gmail.com>
2021-05-02 04:44:02 -0500

    tested python debugger

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ from flask import request
 
 app=Flask(__name__)
 
-@app.route("/")
-def hello():
-    return{
-        "Result": "First Route"
-    }
+# @app.route("/")
+# def hello():
+#     return{
+#         "Result": "First Route"
+#     }
 
 # receive data from Twilio when it hits the endpoint
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    message = request.form["message"]
-    return{
-        "Result": message
-    }
+    # python debugger
+    import pdb
+    # execution stops here and we can analyze the data received here
+    pdb.set_trace()
+    
+    return "OK",200
 
 if __name__=="__main__":
     app.run(debug=True)
